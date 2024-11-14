10bacb0f16bfee146fba4e3f79b56218ee15efc6
Rustik <chuckis94@gmail.com>
2024-11-14 09:47:11 +0200

    task4

--- task4.py
+++ task4.py
@@ -1,23 +1,25 @@
+# n = 10
+# a = [10, 2, 4, 5, -2, 6, 10, -1, 0, -2]
+#----------------------------------------
+n = 4
+a = [-2, 2, 0, -3]
+#----------------------------------------
+curr_max = a[0]
+curr_min = a[n-1]
+curr_ind = 0
+max_ind, min_ind = 0, n
 
-n = 8
-a = [0, -3, 9, 10, 4, 1, 10, -3]
-cmax = a[0]
-cmaxindex = 0 
-cmin = a[n-1]
-cminindex = n-1
-indexarrmax = []
-indexarrmin = []
-print(sum(a))
+print(sum(a)) # TODO
 
 for i in range(n):
-    if a[i] > cmax:
-        cmax = a[i]
-        indexarrmax.append(a.index(cmax))
-
-for i in range(n-1, 0, -1):
-    if a[i] < cmin:
-        cmin = a[i]
-        indexarrmin.append(a.index(cmin))
-
-print(cmax, indexarrmax)
-print(cmin, indexarrmin)
+    curr_ind = i
+    re_ind = n - i # этот индекс для "обратного" прохода по списку
+    if curr_ind > max_ind and a[i] >= curr_max:
+        curr_max = a[i]
+        max_ind = curr_ind
+    if re_ind < min_ind and a[n-i] <= curr_min:
+        curr_min = a[n-i]
+        min_ind = re_ind    
+    
+print(curr_max, max_ind+1)
+print(curr_min, min_ind+1)
